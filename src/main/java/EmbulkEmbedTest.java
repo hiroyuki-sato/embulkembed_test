10f71b1d7a8f97d59261d071913135f9f04eb610
Hiroyuki Sato <hiroysato@gmail.com>
2017-02-13 12:15:54 +0900

    Update example code

--- src/main/java/EmbulkEmbedTest.java
+++ src/main/java/EmbulkEmbedTest.java
@@ -1,30 +1,21 @@
 import java.io.File;
-import java.io.IOException;
-
 import org.embulk.EmbulkEmbed;
 import org.embulk.EmbulkEmbed.Bootstrap;
 import org.embulk.config.ConfigLoader;
 import org.embulk.config.ConfigSource;
 
-
 public class EmbulkEmbedTest {
-
-    public static void main(String args[]) {
-
+    public static void main(String[] args) throws Exception {
         Bootstrap bootstrap = new EmbulkEmbed.Bootstrap();
         EmbulkEmbed embulk = bootstrap.initializeCloseable();
-        String path = "path/to/config/embulk.yml";
+        System.out.println("*** start EmbulkEmbedTest ***");
         try {
             ConfigLoader loader = embulk.newConfigLoader();
-            try {
-                ConfigSource source = loader.fromYamlFile(new File(path));
-                embulk.run(source);
-            } catch(IOException e) {
-                System.out.println("error" + e);
-                System.exit(1);
-            }
+            ConfigSource source = loader.fromYamlFile(new File(args[0]));
+            embulk.run(source);
         } finally {
             embulk.destroy();
         }
     }
 }
+
